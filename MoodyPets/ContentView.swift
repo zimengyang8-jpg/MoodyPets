2f299a24439487df0a93b338755bf3699d786af8
Zimeng Yang <zimengyang8@gmail.com>
2026-04-19 12:21:50 -0400

    Initial Commit

---
+++ MoodyPets/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoodyPets
+//
+//  Created by Zimeng Yang on 4/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoodyPets/MoodyPetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodyPetsApp.swift
+//  MoodyPets
+//
+//  Created by Zimeng Yang on 4/19/26.
+//
+
+import SwiftUI
+
+@main
+struct MoodyPetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
